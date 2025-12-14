77d25991a7f7d5680a2beb9808b9d4ca7be865e1
Anonymous Author <anonymous@users.noreply.github.com>
2025-12-14 23:31:00 +0000

    update full run script

--- ae_cmd.sh
+++ ae_cmd.sh
@@ -1,16 +1,17 @@
-# Figure 5
+# Figure 5 (2m)
 cd /root/step_artifact/
 mv step-perf/src/memory/mod.rs step-perf/src/memory/bw_64_mod.rs
 mv step-perf/src/memory/hdl_mod.rs step-perf/src/memory/mod.rs
 source setup.sh
 pytest hdl_validation/expert_tiling_sweep.py::test_expert_tiling_sweep_single_schedule
-
 mv step-perf/src/memory/mod.rs step-perf/src/memory/hdl_mod.rs
 mv step-perf/src/memory/bw_64_mod.rs step-perf/src/memory/mod.rs
+source setup.sh
+echo "figure 5 done"
 # Produced file: step_artifact/hdl_validation/fig5.csv
 
 
-# Figure 6
+# Figure 6 (30m, 46m)
 cd /root/step_artifact/
 pytest dyn_tiling/test_mixtral_sweep.py::test_mixtral_b64
 # Produced file: step_artifact/dyn_tiling/figure_6_mixtral_b64.csv
@@ -22,8 +23,9 @@ python dyn_tiling/generate_fig6.py
 # Produced file: step_artifact/dyn_tiling/figure6.pdf
 # Produced file: step_artifact/dyn_tiling/figure6.png
 
+echo "figure 6 done"
 
-# Figure 7
+# Figure 7 (72m, 71m)
 cd /root/step_artifact/
 pytest dyn_tiling/test_mixtral_sweep_prefill.py::test_mixtral_b1024
 # Produced file: step_artifact/dyn_tiling/figure_7_mixtral_b1024.csv
@@ -35,24 +37,27 @@ python dyn_tiling/generate_fig7.py
 # Produced file: step_artifact/dyn_tiling/figure7.pdf
 # Produced file: step_artifact/dyn_tiling/figure7.png
 
+echo "figure 7 done"
 
-# Figure 8 & 9
+# Figure 8 & 9 (48m, 52m)
 cd /root/step_artifact/
 pytest timeshare_mem_bound/test_membound_qwen_sweep_revet.py::test_static_tile
 # Produced files: step_artifact/timeshare_mem_bound/fig_8_a.csv, 
 #                 step_artifact/timeshare_mem_bound/fig_9_a.csv,
 #                 step_artifact/timeshare_mem_bound/fig_9_b.csv
-python timeshare_mem_bound/generate_fig8.py 
-# Produced file: step_artifact/timeshare_mem_bound/figure8.pdf
 
 pytest timeshare_mem_bound/test_membound_qwen_sweep_dyn_tile.py::test_dyn_tile
 # Produced files: step_artifact/timeshare_mem_bound/fig_8_b.csv
+
+python timeshare_mem_bound/generate_fig8.py 
+# Produced file: step_artifact/timeshare_mem_bound/figure8.pdf
 python timeshare_mem_bound/generate_fig9.py 
 # Produced file: step_artifact/timeshare_mem_bound/figure9.pdf
 
 
+echo "figure 8 & 9 done"
 
-# Figure 11
+# Figure 11 (1m39s, 4m50s, 6m31s)
 cd /root/step_artifact/
 pytest dynamic_par/sweep_ae.py::test_b16_sweep
 # Produced file: step_artifact/dynamic_par/batch16_sweep_ae.csv
@@ -65,3 +70,4 @@ pytest dynamic_par/sweep_ae.py::test_b64_b16_sweep
 
 python dynamic_par/fig11.py
 # Produced file: step_artifact/dynamic_par/figure11.pdf
+echo "figure 11 done"
